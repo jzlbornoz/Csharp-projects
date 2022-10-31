fcb0f2a7503511757773084cf7cca5d8a26f3e69
Javier Albornoz <javieraap10@gmail.com>
2022-10-31 17:39:14 -0400

    Fundamentos

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Ingresa los lado A para calcular el area: ");
+float number1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Ingresa los lado B: ");
+float number2 = float.Parse(Console.ReadLine());
+float result = number1 * number2;
+Console.WriteLine("Area resultante: " + result + "cm");
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please insert the request information: ");
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("heigth: ");
+            int heigth = Convert.ToInt32(Console.ReadLine());
+            string information = "Hello " + name 
+                + " your data is: \n UserName: " + name 
+                + "\n Age: "+ age + "y/o"
+                + "\n Heigth: " + heigth + "cm";
+            Console.WriteLine(information);
+        }
+    }
+}
