ad14690aa926c8602e8935f0ba448f1ac37a392e
Javier Albornoz <javieraap10@gmail.com>
2022-11-01 19:18:41 -0400

    Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] coffeTypes;
+float[] coffePrices;
+
+coffeTypes = new string[] { "Expreso", "Large", "Medium", "Blackie" };
+coffePrices = new float[] { 1.2f, 2.2f, 1.9f, 1.4f };
+
+for (int i = 0; i < coffeTypes.Length; i++)
+{
+    Console.WriteLine(coffeTypes[i] + " " + coffePrices[i] + "$");
+}
