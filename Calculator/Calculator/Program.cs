eea9ff855897c049e8afcae9dafdcef79f1fb879
Javier Albornoz <javieraap10@gmail.com>
2022-11-01 23:00:09 -0400

    listas, metodos y condicionales

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,6 +1,65 @@
-﻿Console.WriteLine("Ingresa los lado A para calcular el area: ");
-float number1 = float.Parse(Console.ReadLine());
-Console.WriteLine("Ingresa los lado B: ");
-float number2 = float.Parse(Console.ReadLine());
-float result = number1 * number2;
-Console.WriteLine("Area resultante: " + result + "cm");
+﻿using System;
+
+public class Calculator {
+
+    public float Operations(float a, float b, float c)
+    {
+        if (a == 1)
+        {
+            float result = b + c;
+            return result;
+        }
+        else if (a == 2)
+        {
+            float result = b * c;
+            return result;
+        } else
+        {
+           if (a > b)
+            {
+                float result = a / b;
+                return result;
+            } else
+            {
+                float result = b / a;
+                return result;
+            }
+        }
+        return 0;
+    }
+    public static void Main()
+    {
+        Calculator calculator = new Calculator();
+
+        Console.WriteLine("Bienvenido");
+        Console.WriteLine(" 1: Suma \n 2: Multiplicacion \n 3: Divicion");
+        int switchCase = Convert.ToInt32(Console.ReadLine());
+        switch(switchCase)
+        {
+            case 1:
+                Console.WriteLine("Ingresa la primera cantidad: ");
+                float number1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la segunda cantidad: ");
+                float number2 = float.Parse(Console.ReadLine());
+                float result = calculator.Operations(1 ,number1, number2);
+                Console.WriteLine($"Resultado: {result}");
+                break;
+            case 2:
+                Console.WriteLine("Ingresa la cantidad: ");
+                float numberA = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la segunda cantidad: ");
+                float numberB = float.Parse(Console.ReadLine());
+                float resultMult = calculator.Operations( 2 ,numberA, numberB);
+                Console.WriteLine($"Resultado: {resultMult}");
+                break;
+            case 3:
+                Console.WriteLine("Ingresa la cantidad: ");
+                float a = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la segunda cantidad: ");
+                float b = float.Parse(Console.ReadLine());
+                float resultDiv = calculator.Operations(3, a, b);
+                Console.WriteLine($"Resultado: {resultDiv}");
+                break;
+        }
+    }
+}
---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,30 @@
+﻿
+List<string> pastasList = new List<string>();
+
+pastasList.Add("cinco pastas");
+pastasList.Add("cuatro pizzas");
+pastasList.Add("cinco aguas");
+pastasList.Add("dos jugos");
+pastasList.Add("cinco cocas");
+
+for(int i = 0; i < pastasList.Count; i++)
+{
+    Console.WriteLine(i + ". " + pastasList[i]);
+}
+//pastasList.Remove("cinco pastas");
+pastasList.RemoveAt(0);
+
+for (int i = 0; i < pastasList.Count; i++)
+{
+    Console.WriteLine(i + ". " + pastasList[i]);
+}
+
+Random rnd = new Random();
+Console.WriteLine($"{rnd.Next(12, 230)}");
+
+for (int i = 0; i < rnd.Next(12, 150); i++)
+{
+    Console.WriteLine(i);
+}
+
+
